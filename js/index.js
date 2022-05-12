3b5badf72ba44247e620c356e5d7d90302c9d1d2
Cynthia998 <83797264+Cynthia998@users.noreply.github.com>
2022-05-12 20:07:40 +1000

    will edited index.js :)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 const http = require('http');
-
+const { readFile } = require('fs');
+ 
 
 http.createServer((req, res) => {
-    res.write("<b>Hello, World</b>");
-    return res.end();
+    readFile("./home.html", function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(data);
+        }
+    })
 }).listen(5000);
