8997acd7fa23717312ee23b54dc25eb5ff068c3c
theawesomew <theawesomew@gmail.com>
2022-05-19 16:43:30 +1000

    Created upload script

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const mongo = require("mongodb");
 
 const app = express();
 const port = process.env.PORT | "8080";
 const parentDirectory = path.resolve(__dirname, '..');
 
+const uri = "mongodb+srv://sn1p3r:Lazystamp57@howdoesthiswork.p7dya.mongodb.net/?retryWrites=true&w=majority";
+const client = mongo.MongoClient;
+
+/*client.connect(uri, function (err, db) {
+    if (err) throw err;
+});*/
+
+
+
 app.use(express.static(parentDirectory));
+app.use(express.urlencoded());
 
 fs.readdir(parentDirectory, (err, files) => {
     if (err) {
@@ -27,6 +38,10 @@ fs.readdir(parentDirectory, (err, files) => {
 
 app.get("/", (_req, res) => {
     res.redirect("/home");
+});
+
+app.post("/archive", (req, res) => {
+    
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ js/upload_script.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const mongo = require("mongodb");
+const path = require("path");
+
+const uri = "mongodb+srv://sn1p3r:Lazystamp57@howdoesthiswork.p7dya.mongodb.net/?retryWrites=true&w=majority";
+const client = mongo.MongoClient;
+const grid = mongo.GridFSBucket;
+
+client.connect(uri, (err, database) => {
+    if (err) throw err;
+
+    const db = database.db("sample_airbnb");
+    const gridfs = new grid(db);
+
+    fs.readdir('./', (error, files) => {
+        if (error) {
+            console.log(`Could not read the current working directory`);
+            process.exit(1);
+        }
+
+        files.forEach(async (file, _index) => {
+            if (/.*.html/.test(file)) {
+
+                await uploadFile(gridfs, path.resolve(file)).then((done) => {
+                    if (done) {
+                        console.log("File uploaded!");
+                    }
+                }).catch((error) => {
+                    console.log(error);
+                });
+            }
+        })
+    });
+});
+
+function uploadFile (gfs, _path) {
+    return new Promise((resolve, reject) => {
+        let writeStream = gfs.openUploadStream(_path);
+
+        fs.createReadStream(_path).pipe(writeStream);
+
+        writeStream.on("finish", (f) => {
+            if (f.filename) {
+                console.log(`Successfully uploaded file: ${f.filename}`);
+                return resolve(true);
+            } else {
+                return reject(`Failed to upload file`);
+            }
+        });
+    });
+}
